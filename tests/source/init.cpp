cbb529c13925aba0aadd1ccf093a266360c11b04
Alex <kik9399@mail.ru>
2017-05-29 02:24:32 +0300

    getPARENTS

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -19,3 +19,20 @@ SCENARIO ("check","[!mayfail]")
 	
 	REQUIRE (ns::commitList.size() == 25);
 }
+
+SCENARIO ("find_parents") 
+{ 
+char url1[] = { "https://api.github.com/repos/DavidTsyg/Sem2_Lab7/commits?client_id=fcca22e4d885095a01e1&client_secret=5865c57c862238c4e83ea6f97e86ce24e85368fd" }; 
+char filename1[] = {"repo.json"}; 
+
+json j1; 
+commit *a; 
+
+getJSON(url1, filename1); 
+toJSON(j1, filename1, 2); 
+
+a = find_parents(fe232265ca5d35107133c0656ccff49e966b18f0); 
+
+REQUIRE (a == nullptr); 
+
+}
