46e3a55d56ef014e174739b6698c99baef9d64c9
Alex <kik9399@mail.ru>
2017-05-29 03:01:50 +0300

    fix3

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -38,3 +38,18 @@ a = ns::find_parents(shatmp);
 REQUIRE (a == nullptr); 
 }
 
+SCENARIO ("get_parents") 
+{ 
+char url1[] = { "https://api.github.com/repos/DavidTsyg/Sem2_Lab7/commits?client_id=fcca22e4d885095a01e1&client_secret=5865c57c862238c4e83ea6f97e86ce24e85368fd" }; 
+char filename1[] = {"repo.json"}; 
+
+
+json j1; 
+ns::commit c(j1, 0); 
+ns::commit *a = new commit(j1,1); 
+
+getJSON(url1, filename1); 
+toJSON(j1, filename1, 2); 
+ns::get_parents( j1, 25, a) 
+REQUIRE (c.sha == a->parents[0].first); 
+}
