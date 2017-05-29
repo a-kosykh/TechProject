1cc9a8c7a02b941c3cbf32275b5480bf1f559f89
Alex <kik9399@mail.ru>
2017-05-29 03:38:01 +0300

    ver1.0

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -38,9 +38,9 @@ SCENARIO ("find_parents","[!mayfail]")
 	REQUIRE (a == nullptr); 
 }
 
-SCENARIO ("get_parents","[!mayfail]") 
+SCENARIO ("get_parents") 
 { 
-	char url1[] = { "https://api.github.com/repos/DavidTsyg/Sem2_Lab7/commits?client_id=fcca22e4d885095a01e1&client_secret=5865c57c862238c4e83ea6f97e86ce24e85368fd" }; 
+	char url1[] = { "https://api.github.com/repos/DavidTsyg/Sem2_Lab7/comm.." }; 
 	char filename1[] = {"repo.json"}; 
 
 
@@ -50,6 +50,10 @@ SCENARIO ("get_parents","[!mayfail]")
 
 	getJSON(url1, filename1); 
 	toJSON(j1, filename1, 2); 
-	ns::get_parents(j1, 25, a);
-	REQUIRE (c.sha == a->parents.begin()->first); 
+	ns::get_parents( j1, 25, a); 
+	string a ={""}; 
+	for (auto i = a.begin(); i != a.end(); ++i) { 
+		a += (*i).first; 
+	} 
+	REQUIRE (c.sha == a); 
 }
